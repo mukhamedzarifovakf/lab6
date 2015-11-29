3d72d261380541483b1d8a943e7ba18238dcf2ce
mukhamedzarifovakf <mukhamedzarifovakf@gmail.com>
2015-11-30 02:39:09 +0300

    Update A.py

--- A.py
+++ A.py
@@ -1,6 +1,11 @@
-N = int(input())
-n = input().split()
+input = open('input.txt', 'r')
+output = open('output.txt', 'w')
+N, n = input.readlines()
+N = N.rstrip()
+N = int(N)
+n = n.rstrip()
+n = n.split()
 for i in range(N-1):
     for k in range(i+1, N):
         if n[k] == n[i]:
-            print(n[i])
+            print(n[i], file = output)
