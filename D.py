3c5c02da14f5682b9f6a47ad39f6d9ebd3e311b4
mukhamedzarifovakf <mukhamedzarifovakf@gmail.com>
2015-11-30 17:53:57 +0300

    Update D.py

--- D.py
+++ D.py
@@ -1,11 +1,13 @@
-k, n = input().split()
-k = int(k)
-n = int(n)
-Minimums = [int(x) for x in input().split()]
-for i in range (k-1):
-    Current_tempretures = [int(x) for x in input().split()]
+input = open('input.txt', 'r')
+output = open('output.txt', 'w')
+k, n = input.readline().split()
+k, n = int(k), int(n)
+First_year_tempreature = input.readline()
+Minimums = [int(x) for x in First_year_tempreature.split()]
+for i in range (k - 1):
+    Current_year_tempretures = input.readline()
+    Current_tempretures = [int(x) for x in Current_year_tempretures.split()]
     for i in range(n):
         if Minimums[i] > Current_tempretures[i]:
             Minimums[i] = Current_tempretures[i]
-for x in Minimums:
-    print(x, end = ' ')
+print(' '.join(map(str, Minimums)), file = output)
