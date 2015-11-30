1331c2f3bfe41d63bce9cc2bee28ee5209999563
mukhamedzarifovakf <mukhamedzarifovakf@gmail.com>
2015-11-30 19:56:30 +0300

    Update E.py

--- E.py
+++ E.py
@@ -1,12 +1,13 @@
 def CountDebts(Debts, Bills):
     Debts[Bills[0]-1] -= Bills[2]
     Debts[Bills[1]-1] += Bills[2]
-N, K = input().split()
+input = open('input.txt', 'r')
+output = open('output.txt', 'w')
+N, K = input.readline().split()
 K = int(K)
 N = int(N)
 Debts = [0]*N
 for i in range(K):
-    Bills = [int(x) for x in input().split()]
+    Bills = [int(x) for x in input.readline().split()]
     CountDebts(Debts, Bills)
-for x in Debts:
-    print(x, end = ' ')
+print(' '.join(map(str, Debts)), file = output)
